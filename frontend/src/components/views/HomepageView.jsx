5ed8256660601632da841a77ffe937a9f0cca80d
xiqian <keviny413@gmail.com>
2023-03-08 18:03:06 -0500

    changed textfield variant. Added advance search option.

--- frontend/src/components/views/HomepageView.jsx
+++ frontend/src/components/views/HomepageView.jsx
@@ -1,7 +1,32 @@
 import { Box, Typography, TextField } from '@mui/material'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import React from 'react'
 
 export default function HomepageView() {
+  const [open, setOpen] = React.useState(false);
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(Number(event.target.value) || '');
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason !== 'backdropClick') {
+      setOpen(false);
+    }
+  };
   return (
     <> 
       <Box className="home-banner">
@@ -12,8 +37,38 @@ export default function HomepageView() {
         </Box>
        
         <Box sx={{ mt: "5rem", width: "50%" }}>
-          <TextField id="outlined-basic" label="Search for schools" variant="outlined" sx={{ backgroundColor: "white", width: "100%", borderRadius: "5px",}} />
+          <TextField id="outlined-basic" label="Search for schools" variant="filled" sx={{ backgroundColor: "white", width: "100%", borderRadius: "5px",}} />
         </Box>
+        <div>
+          <Button onClick={handleClickOpen} style={{ background: "#2b2d42" }}>Advance Search</Button>
+          <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
+            <DialogTitle>Filter your Search</DialogTitle>
+            <DialogContent>
+              <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel htmlFor="dialog-native">Borough</InputLabel>
+                  <Select
+                    native
+                    value={age}
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Age" id="dialog-native" />}
+                  >
+                    <option aria-label="None" value="" />
+                    <option value={10}>Queens</option>
+                    <option value={20}>Brooklyn</option>
+                    <option value={30}>Bronx</option>
+                    <option value={30}>Manhattan</option>
+                    <option value={30}>Staten Island</option>
+                  </Select>
+                </FormControl>
+              </Box>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose}>Search</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
       </Box>
     </>
   )
