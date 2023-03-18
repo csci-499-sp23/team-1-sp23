1a8f9088f875d897323d26f6f00c2bd82bc4996c
Henry <aosposx@gmail.com>
2023-03-18 17:35:10 -0400

    removed backend-prototype

--- backend/utils/environment.js
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-const port = process.env.PORT;
-const dbName = process.env.DB_NAME;
-const dbURI = process.env.DB_URI;
-
-export { port, dbName, dbURI };
