a433e28a2300f8c3667c08abb75ee5cfdf4b7893
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-03-23 18:22:57 -0400

    added NavBar to MapView.jsx

--- frontend/src/components/views/MapView.jsx
+++ frontend/src/components/views/MapView.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Map from "../Map";
+import Navbar from "./NavBar";
 
 export default function MapView() {
   return (
+    <>
+    <Navbar/>
     <Grid sx={{ height: "100vh" }}>
       <Grid xs={12} sm={8} md={5} item sx={{}}></Grid>
       <Map />
     </Grid>
+    </>
   );
 }
