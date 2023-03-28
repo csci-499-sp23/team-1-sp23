4b364b25b876286a215a9560054597549080ee66
Henry <aosposx@gmail.com>
2023-03-28 17:28:01 -0400

    drawer open close fix

--- frontend/src/components/views/DrawerNavBar.jsx
+++ frontend/src/components/views/DrawerNavBar.jsx
@@ -1,31 +1,16 @@
 import React from "react";
 import { useState } from "react";
-import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
-import Button from "@mui/material/Button";
-import HomeIcon from '@mui/icons-material/Home';
-import PersonIcon from '@mui/icons-material/Person';
-import MapIcon from '@mui/icons-material/Map';
+import { Box, Drawer, List, ListItem, ListItemIcon } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import MapIcon from "@mui/icons-material/Map";
 
-export default function Drawerbar() {
-  const [state, setState] = React.useState({
-    left: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
+export default function Drawerbar({ status, toggle }) {
   return (
     <Box sx={{ width: "100%" }}>
       <Drawer
-        anchor="left"
-        variant="temporary"
-        open={open}
-        onClose={toggleDrawer(false)}
+        open={status}
+        onClose={() => toggle(false)}
         sx={{
           "& .MuiDrawer-paper": {
             width: 60,
@@ -34,29 +19,28 @@ export default function Drawerbar() {
             color: "white",
           },
         }}
-
       >
         <List>
-         <a href="/">
-          <ListItem button>
-            <ListItemIcon>
-              <HomeIcon sx={{color: 'white'}}/>
-            </ListItemIcon>
-          </ListItem>
+          <a href="/">
+            <ListItem>
+              <ListItemIcon>
+                <HomeIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+            </ListItem>
           </a>
           <a href="/profile">
-          <ListItem button>
-            <ListItemIcon>
-              <PersonIcon sx={{color: 'white'}} />
-            </ListItemIcon>
-          </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <PersonIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+            </ListItem>
           </a>
           <a href="/map">
-          <ListItem button>
-            <ListItemIcon>
-              <MapIcon sx={{color: 'white'}} />
-            </ListItemIcon>
-          </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <MapIcon sx={{ color: "white" }} />
+              </ListItemIcon>
+            </ListItem>
           </a>
         </List>
       </Drawer>
