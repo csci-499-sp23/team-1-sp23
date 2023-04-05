d300635e1ae13dd5496d9bfd1a484d7a0910b1de
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-04-05 12:27:57 -0400

    Added some design to the school header - name and address

--- frontend/src/components/views/SchoolpageView.jsx
+++ frontend/src/components/views/SchoolpageView.jsx
@@ -2,18 +2,28 @@ import React, { useEffect } from 'react';
 import NavBar from "./NavBar";
 import { useLocation } from 'react-router-dom';
 
+import { Box, Typography } from "@mui/material";
+import { LocationOn } from '@mui/icons-material';
+
 function SchoolpageView() {
   const location = useLocation();
   const school = location.state.school;
   return (
     <>
     <NavBar/>
-    <div>
-      <h1>{school?.school_name}</h1>
-      <p>Address: {school?.location}</p>
-      <p>Phone: {school?.phone_number}</p>
-    </div>
-    
+    <Box sx={{ bgcolor: '#194973', py: 8 }}>
+      <Typography variant="h4" component="h1" color="common.white" align="left" sx={{ marginLeft: '0.3in', fontFamily: 'Futura', fontSize: 45 }}>
+        {school?.school_name}
+      </Typography>
+      <Typography variant="subtitle1" component="p"  color="common.white" align="left" sx={{ marginLeft: '0.3in', fontSize: 18 }}>
+        <Box component="span" sx={{ verticalAlign: 'middle', marginRight: 1, fontSize: 30, color: 'common.white' }}>
+          <LocationOn />
+        </Box>
+        {school?.location.split('(')[0].trim()} <br />
+        Phone: {school?.phone_number}
+      </Typography>
+    </Box>
+
     </>
   );
 }
