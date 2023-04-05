4166b0ed257beed13453821c8c86e66237733829
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-04-05 12:35:26 -0400

    added navbar to the neighborhood view

--- frontend/src/components/views/SchoolpageView.jsx
+++ frontend/src/components/views/SchoolpageView.jsx
@@ -12,10 +12,10 @@ function SchoolpageView() {
     <>
     <NavBar/>
     <Box sx={{ bgcolor: '#194973', py: 8 }}>
-      <Typography variant="h4" component="h1" color="common.white" align="left" sx={{ marginLeft: '0.3in', fontFamily: 'Futura', fontSize: 45 }}>
+      <Typography variant="h4" component="h1" color="common.white" align="left" sx={{ marginLeft: '1in', fontFamily: 'Futura', fontSize: 45 }}>
         {school?.school_name}
       </Typography>
-      <Typography variant="subtitle1" component="p"  color="common.white" align="left" sx={{ marginLeft: '0.3in', fontSize: 18 }}>
+      <Typography variant="subtitle1" component="p"  color="common.white" align="left" sx={{ marginLeft: '1in', fontSize: 18 }}>
         <Box component="span" sx={{ verticalAlign: 'middle', marginRight: 1, fontSize: 30, color: 'common.white' }}>
           <LocationOn />
         </Box>
