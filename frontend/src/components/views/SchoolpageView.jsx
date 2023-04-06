6d7e3a56cd8ad065a1c996ad084b1a1d06da750a
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-04-06 13:15:22 -0400

    placed headers inside a grid and made code more readable

--- frontend/src/components/views/SchoolpageView.jsx
+++ frontend/src/components/views/SchoolpageView.jsx
@@ -5,44 +5,103 @@ import { useLocation } from 'react-router-dom';
 import { Box, Typography } from "@mui/material";
 import { LocationOn, Phone } from '@mui/icons-material';
 
+import { styled } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import Grid from "@mui/material/Grid";
 
 function SchoolpageView() {
   const location = useLocation();
   const school = location.state.school;
-  
   return (
     <>
     <NavBar />
-    <Box sx={{ bgcolor: '#194973', py: 3.5 }}>
-      <Typography
-        variant="h4"
-        component="h1"
-        color="common.white"
-        align="left"
-        sx={{ display: 'flex', alignItems: 'center', marginLeft: '1in', fontFamily: 'Futura', fontSize: 45, fontWeight: '' }}
-      >
-        {school?.school_name}
-      </Typography>
-      <Typography variant="subtitle1" component="div" color="common.white" align="left" sx={{ marginLeft: '1in', fontFamily: 'Arial', fontSize: 18, display: 'flex', alignItems: 'center' }}>
-        <Box component="span" sx={{ verticalAlign: 'middle', marginRight: 1, fontSize: 30, color: 'common.white' }}>
-          <LocationOn />
-        </Box>
-        <span>{school?.location.split('(')[0].trim()}</span>
-        <Box component="span" sx={{ verticalAlign: 'middle', marginLeft: '1em', fontFamily: 'Arial', fontSize: 39, color: 'common.white' }}>
-          <Phone />
-        </Box>
-        <span style={{marginLeft: '0.4em'}}>Contact Info</span>
-      </Typography>
-    </Box>
-    
-    <Box sx={{ bgcolor: '#255478', py: 1.3 }}>
-      <Typography variant="subtitle2" component="div" align="left" sx={{ marginLeft: '1in', fontFamily: 'Arial', fontSize: 18, display: 'flex', alignItems: 'center' }}>
-        <span>District: <strong style={{color: 'white'}}>#{school?.dbn.slice(0, 2)}</strong></span>
-        <span style={{marginLeft: '6em'}}><strong style={{color: 'white'}}>{school?.total_students}</strong> Students</span>
-        <span style={{marginLeft: '6em'}}>Grades <strong style={{color: 'white'}}>{school?.finalgrades.slice(0, 1)}-{school?.finalgrades.slice(2-4)}</strong></span>
-      </Typography>
-    </Box>
+    <Grid container>
+        <Grid item xs={12} sm={12} md={12}>
+          <Box sx={{ bgcolor: '#194973', py: 3.5 }}>
+            <Typography
+              variant="h4"
+              component="h1"
+              color="common.white"
+              align="left"
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                marginLeft: '1in',
+                fontFamily: 'Futura',
+                fontSize: 45,
+                fontWeight: '',
+                
+              }}
+            >
+              {school?.school_name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              component="div"
+              color="common.white"
+              align="left"
+              sx={{
+                marginLeft: '1in',
+                fontFamily: 'Arial',
+                fontSize: 18,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                component="span"
+                sx={{
+                  verticalAlign: 'middle',
+                  marginRight: 1,
+                  fontSize: 30,
+                  color: 'common.white',
+                }}
+              >
+                <LocationOn />
+              </Box>
+              <span>{school?.location.split('(')[0].trim()}</span>
+              <Box
+                component="span"
+                sx={{
+                  verticalAlign: 'middle',
+                  marginLeft: '1em',
+                  fontFamily: 'Arial',
+                  fontSize: 39,
+                  color: 'common.white',
+                }}
+              >
+                <Phone />
+              </Box>
+              <span style={{ marginLeft: '0.4em' }}>Contact Info</span>
+            </Typography>
+          </Box>
 
+          <Box sx={{ bgcolor: '#255478', py: 1.3 }}>
+            <Typography
+              variant="subtitle2"
+              component="div"
+              align="left"
+              sx={{
+                marginLeft: '1in',
+                fontFamily: 'Arial',
+                fontSize: 18,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <span>
+                District: <strong style={{ color: 'white' }}>#{school?.dbn.slice(0, 2)}</strong>
+              </span>
+              <span style={{ marginLeft: '6em' }}>
+                <strong style={{ color: 'white' }}>{school?.total_students}</strong> Students
+              </span>
+              <span style={{ marginLeft: '6em' }}>
+                Grades <strong style={{ color: 'white' }}>{school?.finalgrades.slice(0, 1)}-{school?.finalgrades.slice(2 - 4)}</strong>
+              </span>
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </>
   );
 }
