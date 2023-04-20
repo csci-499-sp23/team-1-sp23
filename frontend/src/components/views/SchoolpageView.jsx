8c69a8d584dcd1abef78a89e3f6f078146bdf027
Jason <Jason.Wong47@myhunter.cuny.edu>
2023-04-20 00:55:30 -0400

    reverted schoolpageview changes to original

--- frontend/src/components/views/SchoolpageView.jsx
+++ frontend/src/components/views/SchoolpageView.jsx
@@ -1,31 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Box, Grid, List, ListItem, ListItemButton, Typography } from '@mui/material';
 import { LocationOn, Phone, AccessTime, Fax, Email, Language } from '@mui/icons-material';
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import NavBar from "./NavBar";
-
-import {
-  AreaChart,
-  Area,
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-  BarChart,
-  Bar,
-} from 'recharts'
 
 import Iframe from 'react-iframe';
 import { Link } from 'react-router-dom';
@@ -37,16 +14,6 @@ function SchoolpageView() {
   const longitude = Number(school?.longitude);
   const url = `https://www.openstreetmap.org/export/embed.html?bbox=${longitude},${latitude},${longitude},${latitude}&layer=mapnik&marker=${latitude},${longitude}`;
 
-  const [selectedTab, setSelectedTab] = useState(0)
-
-  const handleChange = (e, newValue) => {
-    setSelectedTab(newValue);
-  };
-
-
-  const TabPanel = ({ children, value, index }) => {
-    return value === index && children;
-  };
 
   const urlFix = (schoolUrl) => {
     let fixedUrl = schoolUrl
@@ -186,11 +153,13 @@ function SchoolpageView() {
         <Grid item xs={12} sm={12} md={8}>
           <Box className="middle-container-wrapper">
             <Box className="middle-container school-profile">
+              <h3>School Profile</h3>
               <h2>Overview</h2>
               <p>{school?.overview_paragraph}</p>
             </Box>
     
             <Box className="middle-container school-profile">
+              <h3>School Profile</h3>
               <h2>Schedule and Contact Information</h2>
               <h4>Campus Address</h4>
               <ListItem>
@@ -231,13 +200,13 @@ function SchoolpageView() {
                   <ListItem>
                     <a href={`https://${urlFix(school?.website)}`} target="_blank" rel="noopener noreferrer" style={{ display: 'flex', alignItems: 'center' }}>
                       <Language sx={{ fontSize: '1.5rem', pr: '15px' }} />
-                      <p>{urlFix(school?.website)}</p>
+                      <p>School's Website</p>
                     </a>
                   </ListItem>
                   <ListItem>
                     <a href={`mailto:${school?.school_email}`} style={{ display: 'flex', alignItems: 'center' }}>
                       <Email sx={ { fontSize: '1.5rem', pr: '15px' }} />
-                      <p>{school?.school_email}</p>
+                      <p>School's Email</p>
                     </a>
                   </ListItem>
                 </Box>
@@ -245,6 +214,7 @@ function SchoolpageView() {
             </Box>
 
             <Box className="middle-container school-profile">
+              <h3>School Profile</h3>
               <h2>Navigation</h2>
               <h4>Nearby Transportation</h4>
               <List>
@@ -259,10 +229,7 @@ function SchoolpageView() {
                   </ListItem>
                 )}
               </List>
-              <Link to={{
-                pathname: "/map", 
-                state: {lat: latitude, lng: longitude, school_data: school}
-              }}>
+              <Link to="/map">
               <h4>Click here for more map and direction information</h4>
                 <div className="map-wrapper">
                   <Iframe
@@ -275,187 +242,6 @@ function SchoolpageView() {
                   />
                 </div>
               </Link>
-            </Box>
-            <Box className="middle-container school-profile">
-              <h2>Statistics</h2>
-
-              <Box sx={{
-                display: "flex",
-                flexDirection: {
-                  xs: "column",
-                  sm: "column"
-                },
-              }}>
-                <Tabs
-                  sx={{ alignSelf: "center", mb: 2, pl: 4 }}
-                  value={selectedTab}
-                  onChange={handleChange}
-                >
-                  <Tab sx={{ fontWeight: "500" }} label="Regents Exams Results" />
-                  <Tab sx={{ fontWeight: "500" }} label="AP Exams Results" />
-                  <Tab sx={{ fontWeight: "500"}} label="SAT Exams Results" />
-                </Tabs>
-
-
-                <Box>
-                  <ToggleButtonGroup 
-                    // value={this.state.category} 
-                    // onChange={this.handleCategoryChange} 
-                    exclusive 
-                    sx={{ pr: 4 }}
-                  >
-                    <ToggleButton value="English Proficient">
-                      English Proficient
-                    </ToggleButton>
-                    <ToggleButton value="Former ELL">
-                      Former ELL
-                    </ToggleButton>
-                    <ToggleButton value="ELL">
-                      ELL
-                    </ToggleButton>
-                  </ToggleButtonGroup>
-
-                  <FormControl sx={{ pr: 4 }}>
-                    <InputLabel id="simple-select-label">Exam Type</InputLabel>
-                    <Select
-                      //value={this.state.selectedCategory}
-                      label="Exam"
-                      //onChange={this.handleExamChange}
-                      sx={{ zIndex: 1000 }}
-                    >
-                      {/* {this.state.examTypes.map((exam, index) => {
-                        return (
-                          <MenuItem key={index} value={exam}>
-                            {exam}
-                          </MenuItem>
-                        )
-                      })} */}
-                    </Select>
-                  </FormControl>
-
-                </Box>
-
-              </Box>
-              <TabPanel
-                value={0}
-                index={selectedTab}
-                sx={{ p: 2 }}
-              >
-                {/* {
-                  this.state.regentsData.map(school => {
-                    school.forEach((exam) => {
-                      if (exam.regents_exam == this.state.selectedCategory && exam.category == this.state.category) {
-
-                        this.state.filteredData.push(exam)
-                      }
-                    })
-                  })
-                } */}
-
-                <ResponsiveContainer width="100%" height="100%">
-                  <LineChart
-                    width={500}
-                    height={100}
-                    //data={this.state.filteredData}
-                    margin={{
-                      top: 5,
-                      right: 30,
-                      left: 20,
-                      bottom: 5,
-                    }}
-                  >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="year" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="mean_score" stroke="#8884d8" activeDot={{ r: 8 }} />
-
-                  </LineChart>
-                </ResponsiveContainer>
-              </TabPanel>
-
-              {/* AP DATA TAB */}
-              <TabPanel
-                value={1}
-                index={selectedTab}
-                sx={{ p: 2 }}
-              >
-                {/* {this.state.apData.length !== 0 || "" || undefined ?
-                  <ResponsiveContainer width="100%" height="100%">
-                    <BarChart
-                      width={500}
-                      height={100}
-                      data={this.state.apData[0]}
-                      margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                      }}>
-                      <CartesianGrid strokeDasharray="3 3" />
-                      <XAxis dataKey="" />
-                      <YAxis />
-                      <Tooltip />
-                      <Legend />
-                      <Bar dataKey="num_of_ap_total_exams_taken" fill="#8884d8" />
-                      <Bar dataKey="num_of_ap_exams_passed" fill="#82ca9d" />
-                    </BarChart>
-                  </ResponsiveContainer> :
-                  <Box sx={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}>
-                    <Typography color="text.primary" fontWeight={500}>No data available for this</Typography>
-                  </Box>
-                } */}
-
-              </TabPanel>
-
-              {/* SAT DATA TAB */}
-              <TabPanel
-                value={2}
-                index={selectedTab}
-                sx={{ p: 2 }}
-              >
-                {/* {this.state.apData.length !== 0 || "" || undefined ?
-                  <ResponsiveContainer width="100%" height="100%">
-                    <BarChart
-                      width={500}
-                      height={100}
-                      data={this.state.satData[0]}
-                      margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                      }}>
-                      <CartesianGrid strokeDasharray="3 3" />
-                      <XAxis dataKey="" />
-                      <YAxis />
-                      <Tooltip />
-                      <Legend />
-                      <Bar dataKey="sat_critical_reading_avg_score" fill="#8884d8" />
-                      <Bar dataKey="sat_math_avg_score" fill="#82ca9d" />
-                      <Bar dataKey="sat_writing_avg_score" fill="#23b5d3" />
-                    </BarChart>
-                  </ResponsiveContainer>
-                  : <Box sx={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}>
-                    <Typography color="text.primary" fontWeight={500}>No data available for this</Typography>
-                  </Box>
-                } */}
-              </TabPanel>
             </Box>
           </Box>
         </Grid>
