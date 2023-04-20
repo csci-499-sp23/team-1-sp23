e851ef1be657dbf3a87cc2c0b6e1206e4e3d8df2
Henry <aosposx@gmail.com>
2023-04-20 17:13:26 -0400

    revert vite config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
   commonjsOptions: {
     esmExternals: true,
- },
- build: {
-  rollupOptions:{
-    external: [
-      'react-icons/md', 
-      'react-icons/gi', 
-      'react-icons/sl', 
-      'react-icons/bi', 
-      'react-icons/tb', 
-      'react-icons/hi2', 
-      'react-icons/ri',
-      'react-icons/fa',
-      'react-icons/si',
-      'react-icons/go',
-    ],
-  }
- }
-})
+  },
+});
