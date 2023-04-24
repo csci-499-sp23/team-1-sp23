50192a025cd01f22646f14c2fa3837e259806423
Henry <aosposx@gmail.com>
2023-04-24 19:52:10 -0400

    fixed a bug that caused the search bar and boroughs to overlap the stats

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # team-1-sp23
+
 ## Website Link: [schoolsdb-be6ea.web.app/](https://schoolsdb-be6ea.web.app/)
 
-PR Testing - Ryan
+## Team Members:
+
+- Jason Wong
+- Henry Chen
+- Xiqian Yang
+- Ryan Chevarria
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,39 @@
-import { Routes, Route } from "react-router-dom"
-import { 
-  HomepageContainer, 
-  LoginContainer, 
-  SignupContainer, 
-  MapContainer, 
-  ProfileContainer, 
+import { Routes, Route } from "react-router-dom";
+import {
+  HomepageContainer,
+  LoginContainer,
+  SignupContainer,
+  MapContainer,
+  ProfileContainer,
   NeighborhoodContainer,
   SchoolpageContainer,
-} from './components/containers'
-import Card from "./components/Card"
-import './App.css'
+} from "./components/containers";
+import Card from "./components/Card";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App"> 
+    <div className="App">
       <Routes>
         <Route path="/" element={<HomepageContainer />} />
         <Route path="/signup" element={<SignupContainer />} />
         <Route path="/login" element={<LoginContainer />} />
         <Route path="/map" element={<MapContainer />}>
-          <Route index element={<MapContainer />}/> 
+          <Route index element={<MapContainer />} />
           <Route path="*/filter">
-            <Route path=":borough"/>
+            <Route path=":borough" />
           </Route>
-          <Route path=":place" element={<Card />} /> 
+          <Route path=":place" element={<Card />} />
         </Route>
         <Route path="/profile" element={<ProfileContainer />} />
-        <Route path="/neighborhood/:neighborhood" element ={<NeighborhoodContainer />} />
-        <Route path="/school/:schoolName" element={<SchoolpageContainer/>} />
+        <Route
+          path="/neighborhood/:neighborhood"
+          element={<NeighborhoodContainer />}
+        />
+        <Route path="/school/:schoolName" element={<SchoolpageContainer />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
