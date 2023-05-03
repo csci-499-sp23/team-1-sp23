3eecf0fd4dee1638ef8aaf550e61a15b38f59bd3
Ryan Chevarria <Ryan.Chevarria64@myhunter.cuny.edu>
2023-05-03 16:14:47 -0400

    formatted and added the category, the survey's questions and its respective pie chart with the percentage

--- frontend/src/components/QualityCharts.jsx
+++ frontend/src/components/QualityCharts.jsx
@@ -1,58 +1,91 @@
 import React from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { Table, TableRow, TableCell } from '@mui/material';
 
 const QualityCharts = ({ quality }) => {
-
-    const rigorousInstruction = Number(quality[0]?.rigorousInstruction);
-    const collaborativeTeachers = Number(quality[0]?.collaborativeTeachers);
-    const supportiveEnvironment = Number(quality[0]?.supportiveEnvironment);
-    const effectiveLeadership = Number(quality[0]?.effectiveLeadership);
-    const strongFamilyTies = Number(quality[0]?.strongFamilyTies);
-    const trustPercentage = Number(quality[0]?.trustPercentage);
-
-   
+    const rigorousInstruction = Math.round(quality[0]?.rigorousInstruction);
+    const collaborativeTeachers = Math.round(quality[0]?.collaborativeTeachers);
+    const supportiveEnvironment = Math.round(quality[0]?.supportiveEnvironment);
+    const effectiveLeadership = Math.round(quality[0]?.effectiveLeadership);
+    const strongFamilyTies = Math.round(quality[0]?.strongFamilyTies);
+    const trustPercentage = Math.round(quality[0]?.trustPercentage);
 
     const data = [
-        { category: 'Rigourous Instruction', question: "Do your teachers encourage you to do your best work and explain your thinking in class?", percentage: rigorousInstruction },
-        { category: 'Collaborative Teachers', question: "Do the teachers work together to improve their teaching and are they open to feedback and suggestions from colleagues?", percentage: collaborativeTeachers },
-        { category: 'Safe Environment', question: "Do you feel safe in your classes, hallways, bathrooms, locker rooms, and cafeteria?", percentage: supportiveEnvironment },
+        { category: 'Rigorous Instruction', question: "Do teachers promote excellence and active participation in class?", percentage: rigorousInstruction },
+        { category: 'Collaborative Teachers', question: "Do teachers collaborate and value feedback from colleagues?", percentage: collaborativeTeachers },
+        { category: 'Safe Environment', question: "Do students feel safe, supported, and intellectually stimulated?", percentage: supportiveEnvironment },
         { category: 'Effective Leadership', question: "Are the school's leaders visible, approachable, and trustworthy?", percentage: effectiveLeadership },
-        { category: 'Engaged Families', question: "Does the school provide opportunities for families to learn about their child's progress and volunteer?", percentage: strongFamilyTies },
+        { category: 'Engaged Families', question: "Does the school involve families in progress updates and volunteering?", percentage: strongFamilyTies },
         { category: 'Trust', question: "Does this school have an atmosphere of trust and respect in its relationships?", percentage: trustPercentage },
     ];
 
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#C7B9FF', '#8884d8'];
-    return (
-        <div>
-    {data.map((entry, index) => (
-        <ResponsiveContainer width="100%" height={200} key={entry.category}>
-            <PieChart>
-                <Pie 
-                    data={[
-                        { name: entry.category, value: entry.percentage }, 
-                        { name: ' ', value: 100 - entry.percentage }
-                    ]} 
-                    dataKey="value" 
-                    nameKey="name" 
-                    cx="50%" 
-                    cy="50%" 
-                    outerRadius={70} 
-                    innerRadius={50}
-                >
-                    {[
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />,
-                        <Cell key={`cell-${index}-1`} fill="#F5F5F5" />,
-                    ]}
-                </Pie>
-                <text x="50%" y="50%" textAnchor="middle" dominantBaseline="middle" fontSize="24px">
-                    {entry.percentage}%
-                </text>
-            </PieChart>
-        </ResponsiveContainer>
-    ))}
-</div>
+    const GRAY_COLOR = '#DBDEED';
 
+    const getBarColor = (percentage) => {
+        if (percentage >= 75) {
+            return '#00946a'; //Green
+        } else if (percentage >= 50) {
+            return '#FFBF2A'; //yellow
+        } else {
+            return 'red'; //red
+        }
+    };
+
+    return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Table>
+                <tbody>
+                    {data.map((entry, index) => {
+                        const value = entry.percentage;
+                        const grayValue = 100 - value;
+
+                        const pieData = [
+                            { name: entry.category, value },
+                            { name: '', value: grayValue },
+                        ];
+
+                        return (
+                            <TableRow key={entry.category}>
+                                <TableCell style={{ width: '25%'}}>
+                                    <ResponsiveContainer width="100%" height={90}>
+                                        <PieChart>
+                                            <Pie
+                                                data={pieData}
+                                                dataKey="value"
+                                                nameKey="name"
+                                                cx="50%"
+                                                cy="50%"
+                                                outerRadius={45}
+                                                innerRadius={30}
+                                                startAngle={90}
+                                                endAngle={-270}
+
+                                            >
+                                                <Cell key={`cell-${index}`} fill={getBarColor(entry.percentage)} />
+                                                <Cell key={`cell-${index}-1`} fill={GRAY_COLOR} />
+                                            </Pie>
+                                            <text x="51%" y="46%" textAnchor="middle" dominantBaseline="middle" fontSize="20px">
+                                                {value}%
+                                            </text>
+                                            <text x="50%" y="62%" textAnchor="middle" dominantBaseline="middle" fontSize="14px" fill="#555">
+                                                agree
+                                            </text>
+                                        </PieChart>
+                                    </ResponsiveContainer>
+                                </TableCell>
+                                <TableCell style={{ width: '75%', textAlign: 'left' }}>
+                                    <div style={{ fontSize: '16px', fontWeight: '500' }}>{entry.category}</div>
+                                    <div style={{ fontSize: '14px', color: 'gray' }}>{entry.question}</div>
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        </div>
     );
 };
+
+
 
 export default QualityCharts;
